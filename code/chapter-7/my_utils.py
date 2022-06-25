4420dcce8ca8ba70e0158df37c56cd0346152593
yutingsong <zb-yutingsong@kingmed.com.cn>
2022-06-25 17:46:25 +0800

    feat: 新增训练使用的函数

--- code/chapter-7/my_utils.py
+++ code/chapter-7/my_utils.py
@@ -5,6 +5,7 @@
 @date       : 2022-06-14
 @brief      : 训练所需的函数
 """
+import random
 import numpy as np
 import os
 from matplotlib import pyplot as plt
@@ -12,47 +13,96 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
+from datetime import datetime
+import logging
 
 
-def validate(net, data_loader, set_name, classes_name, device):
-    """
-    对一批数据进行预测，返回混淆矩阵以及Accuracy
-    :param net:
-    :param data_loader:
-    :param set_name:  eg: 'valid' 'train' 'tesst
-    :param classes_name:
-    :return:
-    """
-    net.eval()
-    cls_num = len(classes_name)
-    conf_mat = np.zeros([cls_num, cls_num])
-
-    for data in data_loader:
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-
-        outputs = net(inputs)
-        outputs.detach_()
-
-        _, predicted = torch.max(outputs.data, 1)
-
-        # 统计混淆矩阵
-        for i in range(len(labels)):
-            cate_i = labels[i].cpu().numpy()
-            pre_i = predicted[i].cpu().numpy()
-            conf_mat[cate_i, pre_i] += 1.0
-
-    for i in range(cls_num):
-        print('class:{:<10}, total num:{:<6}, correct num:{:<5}  Recall: {:.2%} Precision: {:.2%}'.format(
-            classes_name[i], np.sum(conf_mat[i, :]), conf_mat[i, i], conf_mat[i, i] / (1 + np.sum(conf_mat[i, :])),
-                                                                conf_mat[i, i] / (1 + np.sum(conf_mat[:, i]))))
-
-    print('{} set Accuracy:{:.2%}'.format(set_name, np.trace(conf_mat) / np.sum(conf_mat)))
-
-    return conf_mat, '{:.2}'.format(np.trace(conf_mat) / np.sum(conf_mat))
+def _weights_init(m):
+    classname = m.__class__.__name__
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
 
 
-def show_conf_mat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=False, perc=False):
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)  # 原版16
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)  # 原版32
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)  # 原版64
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def resnet8(num_classes=10):
+    return ResNet(BasicBlock, [1, 1, 1], num_classes)
+
+
+def show_conf_mat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=False, perc=False, save=True):
     """
     混淆矩阵绘制并保存图片
     :param confusion_mat:  nd.array
@@ -110,7 +160,8 @@ def show_conf_mat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=
             for j in range(confusion_mat_tmp.shape[1]):
                 ax.text(x=j, y=i, s=int(confusion_mat[i, j]), va='center', ha='center', color='red', fontsize=10)
     # 保存
-    fig.savefig(os.path.join(out_dir, "Confusion_Matrix_{}.png".format(set_name)))
+    if save:
+        fig.savefig(os.path.join(out_dir, "Confusion_Matrix_{}.png".format(set_name)))
     plt.close()
 
     if verbose:
@@ -121,3 +172,134 @@ def show_conf_mat(confusion_mat, classes, set_name, out_dir, epoch=999, verbose=
                 confusion_mat[i, i] / (1e-9 + np.sum(confusion_mat[:, i]))))
 
     return fig
+
+
+class ModelTrainer(object):
+
+    @staticmethod
+    def train_one_epoch(data_loader, model, loss_f, optimizer, scheduler, epoch_idx, device, args, logger, classes):
+        model.train()
+
+        class_num = len(classes)
+        conf_mat = np.zeros((class_num, class_num))
+        loss_sigma = []
+        loss_mean = 0
+        acc_avg = 0
+        for i, data in enumerate(data_loader):
+
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            # forward & backward
+            outputs = model(inputs)
+            optimizer.zero_grad()
+
+            loss = loss_f(outputs.cpu(), labels.cpu())
+            loss.backward()
+            optimizer.step()
+
+            # 统计loss
+            loss_sigma.append(loss.item())
+            loss_mean = np.mean(loss_sigma)
+
+            # todo: 采用metrics库进行指标统计
+            _, predicted = torch.max(outputs.data, 1)
+            for j in range(len(labels)):
+                cate_i = labels[j].cpu().numpy()
+                pre_i = predicted[j].cpu().numpy()
+                conf_mat[cate_i, pre_i] += 1.
+
+            acc_avg = conf_mat.trace() / conf_mat.sum()
+
+            # 每10个iteration 打印一次训练信息
+            if i % args.print_freq == args.print_freq - 1:
+                logger.info("Training: Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.4f} Acc:{:.2%}".
+                            format(epoch_idx + 1, args.epochs, i + 1, len(data_loader), loss_mean, acc_avg))
+        return loss_mean, acc_avg, conf_mat
+
+    @staticmethod
+    def evaluate(data_loader, model, loss_f, device, classes):
+        model.eval()
+
+        class_num = len(classes)
+        conf_mat = np.zeros((class_num, class_num))
+        loss_sigma = []
+
+        for i, data in enumerate(data_loader):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = loss_f(outputs.cpu(), labels.cpu())
+
+            # 统计混淆矩阵
+            _, predicted = torch.max(outputs.data, 1)
+            for j in range(len(labels)):
+                cate_i = labels[j].cpu().numpy()
+                pre_i = predicted[j].cpu().numpy()
+                conf_mat[cate_i, pre_i] += 1.
+
+            # 统计loss
+            loss_sigma.append(loss.item())
+
+        acc_avg = conf_mat.trace() / conf_mat.sum()
+
+        return np.mean(loss_sigma), acc_avg, conf_mat
+
+
+class Logger(object):
+    def __init__(self, path_log):
+        log_name = os.path.basename(path_log)
+        self.log_name = log_name if log_name else "root"
+        self.out_path = path_log
+
+        log_dir = os.path.dirname(self.out_path)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+    def init_logger(self):
+        logger = logging.getLogger(self.log_name)
+        logger.setLevel(level=logging.INFO)
+
+        # 配置文件Handler
+        file_handler = logging.FileHandler(self.out_path, 'w')
+        file_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+
+        # 配置屏幕Handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        # console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+        # 添加handler
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+        return logger
+
+
+def make_logger(out_dir):
+    """
+    在out_dir文件夹下以当前时间命名，创建日志文件夹，并创建logger用于记录信息
+    :param out_dir: str
+    :return:
+    """
+    now_time = datetime.now()
+    time_str = datetime.strftime(now_time, '%m-%d_%H-%M-%S')
+    log_dir = os.path.join(out_dir, time_str)  # 根据config中的创建时间作为文件夹名
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    # 创建logger
+    path_log = os.path.join(log_dir, "log.log")
+    logger = Logger(path_log)
+    logger = logger.init_logger()
+    return logger, log_dir
+
+
+def setup_seed(seed=42):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)     # cpu
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True       # 训练集变化不大时使训练加速，是固定cudnn最优配置，如卷积算法
